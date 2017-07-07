f72675c3fefe1884df18bdad8d40791d3abb2180
JDjedi <jcdiaz1201@gmail.com>
2017-07-07 10:44:32 -0500

    More coding challenge work

--- reduction.rb
+++ reduction.rb
@@ -2,26 +2,36 @@
 def dirReduc(direction_array)
 	array_filter = [["NORTH", "SOUTH"], ["SOUTH", "NORTH"], ["WEST", "EAST"], ["EAST", "WEST"]]
 	work_array = []
+	test_directions = []
 
 	while (direction_array.length) > 0
-		test_directions = []
 		if test_directions == []
 			test_directions = direction_array.slice!(0..1)
 			array_filter.each do |x|
-				if x === test_directions
-					test_directions.clear
+				if x == test_directions
+					test_directions = []
+					break
 				else
-					work_array << test_directions[0]
+					work_array << test_directions.slice!(0)
 				end
 			end
 			p direction_array
 			p work_array
-		else
+			puts
+		elsif (test_directions.length) == 1
 			test_directions << direction_array.slice!(0)
-			p direction_array
-			p work_array
+			array_filter.each do |x|
+				if x == test_directions
+					test_directions = []
+					break
+				else
+					work_array << test_directions.slice!(0)
+				end
+			end
 		end
 	end
+	p direction_array
+	p work_array
 end
 
 dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])
